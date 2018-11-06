d8d45449a0012efdc902f5f6db92eaecc5912fb9
Josh Feinstein <pillboxmediainc@gmail.com>
2018-11-06 12:53:10 -0500

    ls

--- bash.js
+++ bash.js
@@ -1,10 +1,13 @@
 const pwd = require('./pwd');
+const ls = require('./ls');
 const prompt = 'prompt > ';
 process.stdout.write(`${prompt}`);
 process.stdin.on('data', data => {
   const cmd = data.toString().trim();
   if (cmd === 'pwd') {
     pwd();
+  } else if (cmd === 'ls') {
+    ls();
   } else {
     process.stdout.write('You typed: ' + cmd);
   }
