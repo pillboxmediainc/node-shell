4e62f9971bf6e251d8e433dbfce87e2f4c25e841
Frank Rose <franklin.rose@gmail.com>
2018-11-06 12:19:31 -0500

    initial commit

---
+++ bash.js
@@ -0,0 +1,7 @@
+const prompt = 'prompt > ';
+process.stdout.write(`${prompt}`);
+process.stdin.on('data', data => {
+  const cmd = data.toString().trim();
+  process.stdout.write('You typed: ' + cmd);
+  process.stdout.write(`\n${prompt}`);
+});
